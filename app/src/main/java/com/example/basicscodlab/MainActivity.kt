939fef9d24e8008eeba6ac42eb80c8a2632a4011
Vadim Korolev <partner380@gmail.com>
2022-12-24 09:12:37 +0300

    State hoisting

--- app/src/main/java/com/example/basicscodlab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodlab/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,9 +30,7 @@ fun Greeting(name: String) {
      *  so the state is not reset.
      */
 
-    val expanded = remember {
-        mutableStateOf(false)
-    }
+    val expanded = remember { mutableStateOf(false) }
 
     /**
      * Don't need to remember [extraPadding] against recomposition
@@ -57,7 +51,12 @@ fun Greeting(name: String) {
                 Text(text = "Hello, ")
                 Text(text = "$name!")
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+            ElevatedButton(
+                /*colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.Red),*/
+                onClick = { expanded.value = !expanded.value }
+            ) {
                 Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
@@ -73,11 +72,67 @@ fun DefaultPreview() {
 }
 
 @Composable
-private fun MyApp(
-    modifier: Modifier = Modifier,
-    name: List<String> = listOf("Word", "Compose")
+private fun MyApp( modifier: Modifier = Modifier ) {
+
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen( onContinueClicked = { shouldShowOnBoarding = false } )
+        } else Greetings()
+    }
+}
+
+@Composable
+private fun OnBoardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            modifier = modifier.padding(24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    BasicsCodlabTheme {
+        OnBoardingScreen(onContinueClicked = {}) // Do nothing onClick.
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier,
+              name: List<String> = listOf("Word", "Compose")
 ) {
     Column( modifier = modifier.padding(vertical = 4.dp) ) {
         name.forEach { Greeting(it) }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingsPreview() {
+    BasicsCodlabTheme {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodlabTheme {
+        MyApp(modifier = Modifier.fillMaxSize())
     }
 }
