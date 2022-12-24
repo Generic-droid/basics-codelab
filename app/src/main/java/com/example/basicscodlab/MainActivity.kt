96f1378d80eb87de66b09444868ae2bea643e08f
Vadim Korolev <partner380@gmail.com>
2022-12-24 18:17:37 +0300

    Finishing touches

--- app/src/main/java/com/example/basicscodlab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodlab/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.basicscodlab
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,65 +39,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    /**
-     * [remember] is used to guard against recomposition,
-     *  so the state is not reset.
-     */
-
-    var expanded by remember { mutableStateOf(false) }
-
-    /**
-     * Don't need to remember [extraPadding] against recomposition
-     * because it's doing a simple calculation.
-     */
-    val extraPadding by animateDpAsState (
-        if (expanded) 48.dp else 0.dp,
-        animationSpec =
-        // tween(1000, easing = CubicBezierEasing(0.4f, 0.0f, 0.8f, 0.8f))
-        spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-        /*repeatable(
-            iterations = 5,
-            animation = tween(1000),
-            repeatMode = RepeatMode.Restart
-        )*/
-    )
-
-    val startColor = Color.Blue
-
-    val endColor = Color.Green
-
-    val backgroundColor by animateColorAsState(
-        if (expanded) endColor else startColor
-    )
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        //color = backgroundColor,
+private fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column( modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+        CardContent(name = name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
             )
-            {
-                Text(text = "Hello, ")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.headlineMedium.copy(
                     fontWeight = FontWeight.ExtraBold
-                ))
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            ElevatedButton(
-                /*colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Blue,
-                    contentColor = Color.Red),*/
-                onClick = { expanded = !expanded }
-            ) {
-                Text(text = if (expanded) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
