cf88303ab0430b015da4214a137c5f72575e7b53
Vadim Korolev <partner380@gmail.com>
2022-12-24 14:58:55 +0300

    Animating your list

--- app/src/main/java/com/example/basicscodlab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodlab/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.basicscodlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodlab.ui.theme.BasicsCodlabTheme
@@ -33,22 +36,44 @@ fun Greeting(name: String) {
      *  so the state is not reset.
      */
 
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
     /**
      * Don't need to remember [extraPadding] against recomposition
      * because it's doing a simple calculation.
      */
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState (
+        if (expanded) 48.dp else 0.dp,
+        animationSpec =
+        // tween(1000, easing = CubicBezierEasing(0.4f, 0.0f, 0.8f, 0.8f))
+        spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+        /*repeatable(
+            iterations = 5,
+            animation = tween(1000),
+            repeatMode = RepeatMode.Restart
+        )*/
+    )
+
+    val startColor = Color.Blue
+
+    val endColor = Color.Green
+
+    val backgroundColor by animateColorAsState(
+        if (expanded) endColor else startColor
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
+        //color = backgroundColor,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column( modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             )
             {
                 Text(text = "Hello, ")
@@ -58,9 +83,9 @@ fun Greeting(name: String) {
                 /*colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Blue,
                     contentColor = Color.Red),*/
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
