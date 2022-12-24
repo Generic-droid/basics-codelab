aa67d665911b901948c5aea329c9baba718930f8
Vadim Korolev <partner380@gmail.com>
2022-12-24 12:21:57 +0300

    Creating a perfomant lazy list

--- app/src/main/java/com/example/basicscodlab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodlab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -114,10 +116,12 @@ fun OnBoardingPreview() {
 
 @Composable
 fun Greetings(modifier: Modifier = Modifier,
-              name: List<String> = listOf("Word", "Compose")
+              names: List<String> = List(1000) {"$it"}
 ) {
-    Column( modifier = modifier.padding(vertical = 4.dp) ) {
-        name.forEach { Greeting(it) }
+    LazyColumn( modifier = modifier.padding(vertical = 4.dp) ) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
     }
 }
 
