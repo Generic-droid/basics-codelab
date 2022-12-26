366e93f9968289dcb77e4fe3c3390f6e05fd59f0
Vadim Korolev <partner380@gmail.com>
2022-12-26 09:01:39 +0300

    Finish codelab

--- app/src/main/java/com/example/basicscodlab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodlab/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.basicscodlab
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,8 +18,6 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
